e451a6744c2ae4c80b01a6c11f226920ffb07399
AlfaroMariena <125149959+AlfaroMariena@users.noreply.github.com>
2023-07-13 12:31:02 -0500

    Modificaciòn en el proyecto(se agrego un modelo Modelo vista controlador

---
+++ Crypto.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crypto.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
