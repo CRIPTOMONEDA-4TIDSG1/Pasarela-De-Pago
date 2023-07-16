94dea99367b629d856a3fbdaac721a854dc43956
rivas <rivas@DESKTOP-TM618MU>
2023-07-16 22:31:33 +0200

    entidad de purchaseorder

---
+++ Crypto.ArqLimpia.EN/PurchaseOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto.ArqLimpia.EN
+{
+    internal class PurchaseOrder
+    {
+        public int Id { get; set; }
+
+        public DateTime DateOrder { get; set; }
+
+        public string HeadLine { get; set; }
+
+        public int SubTotal { get; set; }
+
+        public int Total { get; set; }
+    }
+}
