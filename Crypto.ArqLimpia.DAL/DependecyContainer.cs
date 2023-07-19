82a3d4a96f04a91b07f3c84ce2352c8852b8f5e3
kalet chavez <kalet.svdev@gmail.com>
2023-07-19 14:24:00 -0600

    commit de arreglo

--- Crypto.ArqLimpia.DAL/DependecyContainer.cs
+++ Crypto.ArqLimpia.DAL/DependecyContainer.cs
@@ -20,6 +20,7 @@ namespace Crypto.ArqLimpia.DAL
             options.UseSqlServer($"Server={host};Database={dbName};User Id={user};Password={password};"));
             services.AddScoped<IProduct, ProductDAL>();
             services.AddScoped<IUnitOfWork, UnitOfWork >();
+            
             return services;
         }
         
--- Crypto.ArqLimpia.EN.Interfaces/IUnitOfWork.cs
+++ Crypto.ArqLimpia.EN.Interfaces/IUnitOfWork.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Crypto.ArqLimpia.EN.Interfaces
 {
     public interface IUnitOfWork
