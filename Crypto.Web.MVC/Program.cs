c55c90113f32e545032a990257729d896af4c824
Bryan Misael Sanchez Ramirez <themisael1000@hotmail.com>
2023-07-21 12:23:57 -0600

    Agregacion de RuntimeCompilation

--- Crypto.Web.MVC/Program.cs
+++ Crypto.Web.MVC/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddInventoryDependecies(builder.Configuration);
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
